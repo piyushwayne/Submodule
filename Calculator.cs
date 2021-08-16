b224d5543192ac9cd4f948a521fad4010363d47e
anand.singh@zeiss.com <anand.singh@zeiss.com>
2021-08-16 11:22:25 +0530

    Initial Commit

---
+++ Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Submodule
+{
+    public class Calculator 
+    {
+        double add (double a, double b) => a + b;
+
+        double subtract (double a, double b) => a - b;
+
+        double multiply (double a, double b) => a * b;
+
+        double divide (double a, double b) => a/b;
+    }
+}
